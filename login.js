523b7813c4a72cc08ec56bdc6e797b6c85a11a30
caionobreg <caionobregdev@gmail.com>
2025-11-19 23:05:35 -0300

    Criação da tela Login

---
+++ login.js
@@ -0,0 +1,13 @@
+let card = document.querySelector('.card');
+let loginBtn = document.querySelector('#login-btn');
+let cadastroBtn = document.querySelector('#cadastro-btn');
+
+loginBtn.addEventListener('click', () => {
+    card.classList.remove('cadastro-active');
+    card.classList.add('login-active');
+});
+
+cadastroBtn.addEventListener('click', () => {
+    card.classList.remove('login-active');
+    card.classList.add('cadastro-active');
+});
